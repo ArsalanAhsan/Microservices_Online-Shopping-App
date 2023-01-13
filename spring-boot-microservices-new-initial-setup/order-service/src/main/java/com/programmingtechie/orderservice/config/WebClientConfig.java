fd82ce4b1888988b1b5b31c0ff075a937264da66
Arsalan Ahsan <49119148+ArsalanAhsan@users.noreply.github.com>
2023-01-13 11:32:44 +0500

    Add files via upload

---
+++ spring-boot-microservices-new-initial-setup/order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.programmingtechie.orderservice.config;
+
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClientConfig webClientConfig{
+        return webClientCustomizer.builder.
+    }
+}
